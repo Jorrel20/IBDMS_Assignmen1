9e0cb90d4ff194e0f6b92d5a66b5c7d531d324ce
Jorrel20 <105135055+Jorrel20@users.noreply.github.com>
2023-05-01 11:26:20 +1000

    commit

---
+++ src/Client/Drone.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Client;
+
+/**
+ *
+ * @author Jorrel
+ */
+public class Drone {
+      private String Name;
+      private String ID;
+      private int x;
+      private int y;
+      private boolean registered = false;
+      private boolean recalled = false;
+        
+  //Constructor
+ public Drone(){
+     Scanner scanner = new Scanner(System.in);
+ }
+}

bf6215157f8f4b6a5163e72172591d83c7cd0792
Jorrel20 <105135055+Jorrel20@users.noreply.github.com>
2023-05-01 10:27:07 +1000

    commit

---
+++ src/ibdms/IBDMS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ibdms;
+
+/**
+ *
+ * @author Jorrel
+ */
+public class IBDMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
